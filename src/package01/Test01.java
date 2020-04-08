8653195492d38372c7893c3bddd83c78e8370899
刘鹏 <46567390+QQ1215465821@users.noreply.github.com>
2020-04-09 00:16:51 +0800

    Update Test01.java

--- src/package01/Test01.java
+++ src/package01/Test01.java
@@ -7,5 +7,6 @@ public class Test01 {
         System.out.println("主干输出");
         System.out.println("主干再次输出");
         System.out.println("idea上操作");
+        System.out.println("github上操作");
     }
 }
